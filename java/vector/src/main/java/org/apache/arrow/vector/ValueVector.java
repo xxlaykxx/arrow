b83695111dbad4cf80f702a87537b3ba0a8d2118
Siddharth Teotia <siddharth@dremio.com>
2017-10-14 11:38:30 -0400

    Cherry-Pick ARROW-1473 and ARROW-1474: New ValueVector Implementation

--- java/vector/src/main/java/org/apache/arrow/vector/ValueVector.java
+++ java/vector/src/main/java/org/apache/arrow/vector/ValueVector.java
@@ -255,4 +255,18 @@ public interface ValueVector extends Closeable, Iterable<ValueVector> {
    * @return buffer
    */
   public ArrowBuf getOffsetBuffer();
+
+  /* temporarily add these methods here until we remove other vectors
+   * (non-nullable) which are under ValueVector hierarchy and still
+   * use the mutator/accessor interfaces.
+   */
+  public int getValueCount();
+
+  public void setValueCount(int valueCount);
+
+  public Object getObject(int index);
+
+  public int getNullCount();
+
+  public boolean isNull(int index);
 }
