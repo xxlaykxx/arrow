fa76519820195b0334179ba4d5b3ddd7790e1b4f
David Li <li.davidm96@gmail.com>
2025-08-05 12:37:33 +0900

    MINOR: Fix format (#809)
    ## What's Changed
    
    Apply pre-commit since I forgot.
    
    (cherry picked from commit e4f64269db0a08299fa25be491570be5ba71d623)

--- java/vector/src/main/codegen/templates/UnionListWriter.java
+++ java/vector/src/main/codegen/templates/UnionListWriter.java
@@ -340,12 +340,12 @@ public class Union${listName}Writer extends AbstractFieldWriter {
     writer.writeExtension(value);
     writer.setPosition(writer.idx() + 1);
   }
-  
+
   @Override
   public void addExtensionTypeWriterFactory(ExtensionTypeWriterFactory var1) {
     writer.addExtensionTypeWriterFactory(var1, extensionType);
   }
-  
+
   public void write(ExtensionHolder var1) {
     writer.write(var1);
   }
